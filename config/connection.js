b886843b6a02e62e0b0503db6d142b9025bc9257
Harold Zuluaga <haroldzuluaga@aol.com>
2020-11-06 18:11:04 -0800

    update msql string

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,12 @@ var mysql = require("mysql");
 var connection;
 if (process.env.JAWSDB_PURPLE_URL) {
   // Database is JawsDB on Heroku
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection({
+      host: "nuskkyrsgmn5rw8c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      user: "s3a3n64bw9csptd0",
+      password: "rz9dq88fjjw15jte",
+      database: "g9ors53of9pr8fpf"
+  });
 } else {
   // Database is local
   connection = mysql.createConnection({
